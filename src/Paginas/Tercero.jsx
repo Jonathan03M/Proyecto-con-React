a67529572d223c9c83913138d3b15f88a22ac180
Jonathan03M <yoninplay03@hotmail.com>
2022-11-07 16:47:31 -0300

    commit numero 590

--- src/Paginas/Tercero.jsx
+++ src/Paginas/Tercero.jsx
@@ -1,17 +1,40 @@
 import React from 'react'
 import Sidebar3 from './components/Sidebar3'
+import {DatePicker} from '@material-ui/pickers'
+import '../Paginas/components/Tercero.css'
+import {TimePicker} from '@material-ui/pickers'
+import {DateTimePicker} from '@material-ui/pickers'
 
 function Tercero() {
+ 
+
 return (
 
     <div className="grid lg:grid-cols-4 xl:grid-cols-6 min-h-screen">
     <Sidebar3/>
     
     
-    <main className="flex items-center lg:col-span-3 xl:col-span-5 bg-gray-100 p-20  overflow-y-scroll">
+    <main className="flex items-center lg:col-span-3 xl:col-span-5 bg-gray-100 p-21  overflow-y-scroll">
 
 
 <section className='container d-flex justify-content-center align-items-center h-100'>
+
+    <div className='contenedor'>
+        <div className='grupo'>
+            <label>Fecha</label>
+            <DatePicker/>
+        </div>
+        <div className='grupo'>
+            <label>Hora</label>
+            <TimePicker/>
+        </div>
+        <div className='grupo'>
+            <label>Fecha y Hora</label>
+            <DateTimePicker/>
+        </div>
+
+    </div>
+
 </section>
 </main>
 </div>
